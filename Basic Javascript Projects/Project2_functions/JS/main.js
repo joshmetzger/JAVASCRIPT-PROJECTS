957f6d78dd693128d5c189ef8cc3a7f542aa8c80
joshmetzger <75275288+joshmetzger@users.noreply.github.com>
2025-01-09 15:34:40 -0800

    added ex 2

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+var word1 = "this here,"
+var word2 = " over there"
+
+function thisFunction(){
+    console.log(word1 + word2)
+}
+
+function thisFunction2(){
+    document.getElementById('thisButton').innerHTML = 'clicked'
+}
