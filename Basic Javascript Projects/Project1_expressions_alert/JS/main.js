775f7012c9736cb55bf7c7367b3059443808fb93
joshmetzger <75275288+joshmetzger@users.noreply.github.com>
2025-01-09 15:03:24 -0800

    initial commit

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+// make variable with string
+var junt = "this is \"another\" string" + " ADDITIONALLY THIS STRINGDFFDFG"
+
+// here is a popup window
+window.alert(junt)
+
+// another variable string
+var A = "this is a string"
+
+// same
+var Dog1 = 'Ivan', Dog2 = 'Mega', Dog3 = 'Evie';
+
+// wrting things to the webpage
+document.write(A);
+document.write(Dog3);
+
+// concatinate strings with variables
+var Sent1 = "This is the beginning of the string"
+var Sent2 = " and this is the end of the string"
+var combined = Sent1 + Sent2
+
+// print to page
+document.write(combined);
+
+500 + 600
+
+
