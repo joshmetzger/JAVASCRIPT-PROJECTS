2a95194c8fb31740b64f67614b11ab7c817efb5b
joshmetzger <75275288+joshmetzger@users.noreply.github.com>
2025-01-09 15:42:22 -0800

    functions index.html updated

--- Basic Javascript Projects/Project2_functions/JS/main.js
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -1,10 +1,17 @@
+// create strings to concatenate
 var word1 = "this here,"
 var word2 = " over there"
 
+// concatenate another string with operator
+var sentence = "the quick brown fox "
+sentence += "jumped over lil dog";
+
+// log String, concat in the log itself
 function thisFunction(){
     console.log(word1 + word2)
 }
 
+// display concatenated string with operator, on the page from button click
 function thisFunction2(){
-    document.getElementById('thisButton').innerHTML = 'clicked'
+    document.getElementById('thisButton').innerHTML = sentence
 }
