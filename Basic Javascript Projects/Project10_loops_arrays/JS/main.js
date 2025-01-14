0cf787d8ddd19207dc60fc2f76f04ac92767c569
joshmetzger <75275288+joshmetzger@users.noreply.github.com>
2025-01-14 12:14:13 -0800

    updated JS10

--- Basic Javascript Projects/Project10_loops_arrays/JS/main.js
+++ Basic Javascript Projects/Project10_loops_arrays/JS/main.js
@@ -18,3 +18,72 @@ function array_Function(){
     
     document.getElementById('Array').innerHTML = instruments[1] + ' is not an instrument, but a ' + instruments[3] + ' is.'
 }
+
+const food = {veggie: 'potato', fruit: 'apple', protein: 'chicken'};
+food.veggie = 'broccoli';
+food.grain = 'rice';
+function constant_Function(){
+    
+    // document.getElementById('Constant').innerHTML = food.protein;
+    document.getElementById('Constant').innerHTML = food.veggie;
+
+    // document.getElementById('Constant').innerHTML = food.grain;
+
+}
+
+// experiment with scope and LET
+{
+    let str = 'changed';
+    console.log(str);
+}
+console.log(str);
+
+function ret(){
+    return str;
+}
+
+console.log(ret());
+
+let person = {
+    age: 33,
+    yearsAtJob: 5,
+    percentOfLifeAtJob : function(){
+        return (this.age / this.yearsAtJob * 100);
+    }
+};
+document.getElementById('person').innerHTML = person.percentOfLifeAtJob();
+
+
+// stops at 4
+let A = 10;
+
+while(A > 0){
+    if(A === 4){
+        break;
+    }
+    A = A - 1;
+    console.log(A)
+
+}
+
+// let B = 0;
+
+// while(B < 10){
+//     if(B === 4){
+//         continue;
+//     }
+//     B = B + 1;
+//     console.log(B)
+
+// }
+
+// basically just skips 3
+let text = '';
+for (let i = 0; i < 10; i++) {
+    if (i === 3) { continue; }
+    text += "The number is " + i + "<br>";
+}
+
+console.log(text)
+
+
